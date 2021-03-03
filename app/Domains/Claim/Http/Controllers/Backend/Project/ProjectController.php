71937080091516c19c4bc64e0b7360b7ccc42641
Mahammadali <manknojiya121@gmail.com>
2021-03-03 13:51:05 +0530

    Testing all cases for partners and project and cost items relationship

--- app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
+++ app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
@@ -118,6 +118,7 @@ class ProjectController
             $data = [];
             if(empty(request()->partner)) {
                 $costItems = $project->costItems->whereNull('project_cost_items.deleted_at')->groupBy('pivot.cost_item_id')->all();
+                dd($costItems);
                 foreach ($costItems as $key => $costItem) {
                     $quarterDates = [];
                     if(!empty($costItem)) {
