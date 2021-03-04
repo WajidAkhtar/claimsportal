1dc091a1290042ff8a9c66bb5a033fe5361eb923
Mahammadali <manknojiya121@gmail.com>
2021-03-04 17:16:49 +0530

    Changes

--- resources/views/backend/claim/project/show-yearwise-master.blade.php
+++ resources/views/backend/claim/project/show-yearwise-master.blade.php
@@ -131,7 +131,7 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text readonly">£</span>
                         </div>
-                        {{ html()->input('number', 'yearly_data['.$yearIndex.'][claim_values]['.$costItem->id.'][variance]',(optional($data->claims_data[$costItem->id])['yearwise'])[$i]['budget'])
+                        {{ html()->input('number', 'yearly_data['.$yearIndex.'][claim_values]['.$costItem->id.'][variance]',(!empty($data->claims_data[$costItem->id])) ? $data->claims_data[$costItem->id]['yearwise'][$i]['budget'] : 0.00 )
                             ->placeholder('0.00')
                             ->class('form-control')
                             ->readOnly()
