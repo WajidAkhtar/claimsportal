15b5b9b0f2be63dbf8835ff92c03c1bd47e6a13a
Mahammadali <manknojiya121@gmail.com>
2021-03-04 16:36:54 +0530

    Changes

--- resources/views/backend/claim/project/show-master.blade.php
+++ resources/views/backend/claim/project/show-master.blade.php
@@ -622,6 +622,20 @@
                 $(v).closest('table').find('[name^="yearly_data['+yearIndex+'][total_costs][cumulative]['+rowId+']"]').val(cumulativeTotal.toFixed(2));
             });
 
+            $('[name ^="yearly_data["][name $="[total_costs][for_each_item][variance]"]').not('[name*="[yearwise]"]').each(function(i, v) {
+                var total_project_variance = 0;
+                var yearIndex = $(v).attr('name').match(/(?<=\[).*?(?=\])/g)[0];
+                $('[name^="yearly_data['+yearIndex+'][claim_values]"][name$="[variance]"]').each(function(i1, v1) {
+                    if($(v1).val() == '' || isNaN($(v1).val())) {
+                        value = 0;
+                    } else {
+                        value = $(v1).val();
+                    }
+                    total_project_variance += parseFloat(value);
+                });
+                $('[name="yearly_data['+yearIndex+'][total_costs][for_each_item][variance]"]').val(total_project_variance.toFixed(2));
+            });
+
             $('#year-wise-claims [name$="[for_each_item][project_total]"]').each(function(i, v){
                 var yearIndex = $(v).attr('name').match(/(?<=\[).*?(?=\])/g)[0];
                 var project_total = 0;
@@ -638,20 +652,6 @@
 
                 var total_budget = $(v).closest('tr').find('[name*="yearly_data['+yearIndex+'][total_costs][for_each_item][total_budget]"]').val();
                 $(v).closest('tr').find('[name$="[for_each_item][variance]"]').val((total_budget - project_total).toFixed(2))
-            });
-
-            $('[name ^="yearly_data["][name $="[total_costs][for_each_item][variance]"]').not('[name*="[yearwise]"]').each(function(i, v) {
-                // var total_project_variance = 0;
-                // var yearIndex = $(v).attr('name').match(/(?<=\[).*?(?=\])/g)[0];
-                // $('[name^="yearly_data['+yearIndex+'][claim_values]"][name$="[variance]"]').each(function(i1, v1) {
-                //     if($(v1).val() == '' || isNaN($(v1).val())) {
-                //         value = 0;
-                //     } else {
-                //         value = $(v1).val();
-                //     }
-                //     total_project_variance += parseFloat(value);
-                // });
-                // $('[name="yearly_data['+yearIndex+'][total_costs][for_each_item][variance]"]').val(total_project_variance.toFixed(2));
             });
 
             $('[name^="yearly_data"][name$="[total_costs][for_each_item][total_budget]"]').each(function(i, v){
