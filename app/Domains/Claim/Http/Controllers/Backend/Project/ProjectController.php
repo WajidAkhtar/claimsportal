1caf8c202e76b09c7b3071ff7982b0c4bb949166
Mahammadali <manknojiya121@gmail.com>
2021-03-19 13:39:30 +0530

    Changes

--- app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
+++ app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
@@ -66,8 +66,8 @@ class ProjectController
      */
     public function create()
     {
-        Organisation::truncate();
-        OrganisationSeeder::run();
+        // Organisation::truncate();
+        // OrganisationSeeder::run();
         dd(DB::table('organisations')->get(), Organisation::ordered()->get(), Organisation::withTrashed()->get());
         if(!auth()->user()->hasRole('Administrator') && !auth()->user()->hasRole('Super User') && !auth()->user()->hasRole('Finance Officer') && !auth()->user()->hasRole('Project Admin')) {
             return redirect()->route('admin.claim.project.index')->withFlashDanger(__('you have no access to create project.'));
