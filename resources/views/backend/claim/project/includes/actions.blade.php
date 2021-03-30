71c6824c8f947b13e08919b67be0e0cdeec3021c
Ali <manknojiya121@gmail.com>
2021-03-30 18:01:42 +0530

    Project Lead Conditions

--- resources/views/backend/claim/project/includes/actions.blade.php
+++ resources/views/backend/claim/project/includes/actions.blade.php
@@ -1,7 +1,7 @@
 <x-utils.view-button :href="route('admin.claim.project.show', $project)" />
-@if($project->created_by == auth()->user()->id || auth()->user()->hasRole('Super User') || auth()->user()->hasRole('Administrator') || (projectLead($project)->id == auth()->user()->id))
+@if($project->created_by == auth()->user()->id || auth()->user()->hasRole('Super User') || auth()->user()->hasRole('Administrator') || (!empty(projectLead($project)) && (projectLead($project)->id == auth()->user()->id)))
 	<x-utils.edit-button :href="route('admin.claim.project.edit', $project)" />
-	@if(auth()->user()->hasRole('Administrator') || (projectLead($project)->id == auth()->user()->id))
+	@if(auth()->user()->hasRole('Administrator') || (!empty(projectLead($project)) && (projectLead($project)->id == auth()->user()->id)))
 	<x-utils.delete-button :href="route('admin.claim.project.destroy', $project)" />
 	@endif
 @endif
