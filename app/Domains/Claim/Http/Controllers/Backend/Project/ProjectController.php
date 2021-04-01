018960a2bedde97264373297cdaf246ada400bed
Ali <manknojiya121@gmail.com>
2021-04-01 09:45:55 +0530

    Default cost item data change

--- app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
+++ app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
@@ -70,6 +70,98 @@ class ProjectController
      */
     public function create()
     {
+        CostItem::truncate();
+        CostItem::create([
+            'name' => 'A1',
+            'description' => 'Directly Incurred: Staff',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+        
+        CostItem::create([
+            'name' => 'A2',
+            'description' => 'Directly Incurred: Travel & Subsistence',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+        
+        CostItem::create([
+            'name' => 'A3',
+            'description' => 'Directly Incurred: Equipment',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A4',
+            'description' => 'Directly Incurred: Consumables',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A5',
+            'description' => 'Directly Allocated: Investigators',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A6',
+            'description' => 'Directly Allocated: Estates',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A7',
+            'description' => 'Directly Allocated: Other Costs',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A8',
+            'description' => 'Indirect Costs',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A9',
+            'description' => 'Exceptions: Staff',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A10',
+            'description' => 'Exceptions: Travel & Subsistence',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A11',
+            'description' => 'Exceptions: Other',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A12',
+            'description' => 'Exceptions: Student Internships',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+
+        CostItem::create([
+            'name' => 'A13',
+            'description' => 'Exceptions:',
+            'active' => true,
+            'is_system_generated' => true,
+        ]);
+        dd(1);
         if(!auth()->user()->hasRole('Administrator') && !auth()->user()->hasRole('Super User') && !auth()->user()->hasRole('Finance Officer') && !auth()->user()->hasRole('Project Admin')) {
             return redirect()->route('admin.claim.project.index')->withFlashDanger(__('you have no access to create project.'));
         }
