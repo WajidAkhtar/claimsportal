243d3f6df3ade70cd9dfedc13dca23c273a27ff3
Ali <manknojiya121@gmail.com>
2021-04-03 10:02:59 +0530

    Edit profile page - Issue fixed of showing error when clicking on Cancel button

--- resources/views/backend/auth/user/edit.blade.php
+++ resources/views/backend/auth/user/edit.blade.php
@@ -14,7 +14,7 @@
             </x-slot>
 
             <x-slot name="headerActions">
-                <x-utils.link class="card-header-action" :href="route('admin.auth.user.index')" :text="__('Cancel')" />
+                <x-utils.link class="card-header-action" :href="route(homeRoute())" :text="__('Cancel')" />
             </x-slot>
 
             <x-slot name="body">
