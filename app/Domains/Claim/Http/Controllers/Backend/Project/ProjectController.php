0df82b3fea90c1d29e05b70b382aae49b2b4b8ad
Mahammadali <manknojiya121@gmail.com>
2021-04-14 08:40:00 +0530

    Debugging export issue

--- app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
+++ app/Domains/Claim/Http/Controllers/Backend/Project/ProjectController.php
@@ -202,8 +202,8 @@ class ProjectController
             $partnerAdditionalInfo = ProjectPartners::where('project_id', $project->id)->where('is_master', '1')->first();
             $SheetUserPermissions = SheetUserPermissions::where('project_id', $project->id)->where('is_master', '1')->get();
 
-            $data = (object) $data;
             dd($data);
+            $data = (object) $data;
             $yearwiseHtml = View::make('backend.claim.project.show-yearwise-master', ['project' => $project, 'data' => $data])->render();
 
             if(!empty(request()->exportExcel)) {
