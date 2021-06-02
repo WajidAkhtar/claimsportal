aa6b87c730f4d6bbd8205c182f23cfa0bb8ed34f
Mahammadali <manknojiya121@gmail.com>
2021-06-02 10:22:34 +0530

    Forgot Password Page UI changes

--- resources/views/frontend/auth/passwords/email.blade.php
+++ resources/views/frontend/auth/passwords/email.blade.php
@@ -2,27 +2,35 @@
 
 @section('title', __('Reset Password'))
 
+<style type="text/css">
+    .card {
+        border: none !important;
+    }
+</style>
+
 @section('content')
-    <div class="container py-4">
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-4">
                 <x-frontend.card>
-                    <x-slot name="header">
-                        @lang('Reset Password')
-                    </x-slot>
-
                     <x-slot name="body">
+                        <div class="c-sidebar-brand mb-4" style="justify-content: left !important;">
+                            <img src="{{ asset('assets/backend/images/logo_horiz_white.jpg') }}" class="sidebar-logo" height="100">
+                        </div>
                         <x-forms.post :action="route('frontend.auth.password.email')">
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">@lang('E-mail Address')</label>
-
-                                <div class="col-md-6">
-                                    <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" placeholder="{{ __('E-mail Address') }}" maxlength="255" required autofocus autocomplete="email" />
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <svg class="c-icon">
+                                            <use xlink:href="{{ asset('assets/backend/vendors/@coreui/icons/svg/free.svg#cil-user') }}"></use>
+                                        </svg>
+                                    </span>
                                 </div>
-                            </div><!--form-group-->
+                                <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" placeholder="{{ __('E-mail Address') }}" maxlength="255" required autofocus autocomplete="email" />
+                            </div>
 
                             <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
+                                <div class="col-md-12">
                                     <button class="btn btn-primary" type="submit">@lang('Send Password Reset Link')</button>
                                 </div>
                             </div><!--form-group-->
